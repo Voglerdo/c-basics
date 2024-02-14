4afa82f858b3f7f91b3941371920834fa0314d7c
Dominik <120841465+Voglerdo@users.noreply.github.com>
2024-02-14 18:39:31 +0000

    switch case

--- main.cpp
+++ main.cpp
@@ -15,10 +15,36 @@ void forLoop(int loops){
   }
 }
 
+void weekdayPrinter(int weekDay){
+  switch (weekDay) {
+  case 1:
+    cout << "Monday";
+    break;
+  case 2:
+    cout << "Tuesday";
+    break;
+  case 3:
+    cout << "Wednesday";
+    break;
+  case 4:
+    cout << "Thursday";
+    break;
+  case 5:
+    cout << "Friday";
+    break;
+  case 6:
+    cout << "Saturday";
+    break;
+  case 7:
+    cout << "Sunday";
+    break;
+}
+}
 
 int main() {
   whilePrinter(8);
   forLoop(5);
+  weekdayPrinter(3);
   return 0;
 }
 
